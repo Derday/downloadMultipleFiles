57223191b06f2268adb7ca5f991b99da2d8060c1
Derday <dada.hofm@gmail.com>
2023-03-29 23:27:25 +0200

    Possibility of addition urls while running

--- download.py
+++ download.py
@@ -1,27 +1,42 @@
+from time import sleep
 from uldlib.downloader import Downloader
 from uldlib.frontend import ConsoleFrontend
 from uldlib.captcha import AutoReadCaptcha
 from pathlib import Path
 import os
-from os.path import isfile, join
+import shutil
 
 class Download:
 
     def __init__(self) -> None:
         self.path = Path(os.path.dirname(__file__))
         self.tempFolder = self.path.joinpath('temp')
+        self.index = 0
     
     def fromFile(self, file:str = 'input.txt') -> None:
         """
         Download files from urls, that are in `input file`
         """
-        with open(self.path.joinpath(file), 'r') as f:
-            urls = f.readlines()
-        urls = list(map(lambda str: str.rstrip(), urls))
-        for url in urls:
-            self.download(url, 50)
+        while True:
+            line = self._nextLine(self.path.joinpath(file))
+            if line:
+                self.download(line, 50)
+            else:
+                break
+    
+    def _nextLine(self, path:Path) -> str:
+        with open(path, 'r') as f:
+            lines = f.readlines()
+        self.index += 1
+        try:
+            if lines[self.index-1] == '\n':
+                line = self._nextLine(path)
+            line = lines[self.index-1].rstrip()
+        except:
+            line = None
+        return line
 
-    def download(self, url: str, parts: int) -> None:
+    def download(self, url: str, parts: int = 25) -> None:
         """
         Downloads file from url
         """
@@ -39,12 +54,16 @@ class Download:
         Move downloaded files to different folder, removes the remaining `.ucache` and `.udown` files.
         """
         out = outFolder or self.path.joinpath('downloaded')
-        os.system(f'rm {self.tempFolder.joinpath("*.ucache")}')
-        os.system(f'rm {self.tempFolder.joinpath("*.udown")}')
-        files = [f for f in os.listdir(self.tempFolder) if isfile(join(self.tempFolder, f))]
+        if not out.exists():
+            out.mkdir(parents = False, exist_ok = False)
+        files = os.listdir(self.tempFolder)
         files.remove('model.tflite')
         for file in files:
-            os.system(f'mv {self.tempFolder}/"{file}" {out}')
+            if file.endswith('.ucache') or file.endswith('*.udown'):
+                os.remove(os.path.join(files, file))
+            else:
+                shutil.move(self.tempFolder.joinpath(file), out)
+
 
 
 if __name__ == "__main__":
